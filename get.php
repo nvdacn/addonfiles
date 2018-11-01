624566c895e78b0b4b0623acc1f60f7b388b6e2d
dingpengyu <26911141+dingpengyu@users.noreply.github.com>
2018-11-02 07:09:36 +0800

    add-NVDA-plugin

---
+++ get.php
@@ -0,0 +1,132 @@
+<?php
+$addons = array(
+	"ath" => "https://github.com/mush42/Audio-Themes-NVDA-Add-on/releases/download/v5.1/AudioThemes3D-5.1.nvda-addon",
+	"access8math" => "https://github.com/tsengwoody/Access8Math/releases/download/v1.5/Access8Math-1.5.nvda-addon",
+	"ath-dev" => "https://github.com/mush42/Audio-Themes-NVDA-Add-on/releases/download/v5.1/AudioThemes3D-5.1.nvda-addon",
+	"bc" => "bitChe-2.7.nvda-addon",
+	"bc-dev" => "bitChe-2.7.nvda-addon",
+	"bgt" => "https://github.com/sykesman/NVDA-BGT/releases/download/1.0-dev/bgt_lullaby-1.0-dev.nvda-addon",
+	"brlext" => "https://andreabc.net/projects/NVDA_addons/BrailleExtender/latest",
+	"brlext-dev" => "https://andreabc.net/projects/NVDA_addons/BrailleExtender/latest?channel=dev",
+	"cae" => "calibre-1.0.nvda-addon",
+	"cae-dev" => "calibre-dev20181031.nvda-addon",
+	"clsel" => "https://github.com/tspivey/classicSelection/releases/download/v1.0/classicSelection-1.0.nvda-addon",
+	"ccd" => "https://github.com/nvdaes/clipContentsDesigner/releases/download/8.4/clipContentsDesigner-8.4.nvda-addon",
+	"ccd-dev" => "https://github.com/nvdaes/clipContentsDesigner/releases/download/8.4/clipContentsDesigner-8.4.nvda-addon",
+	"ccd-o" => "https://github.com/nvdaes/clipContentsDesigner/releases/download/7.2/clipContentsDesigner-7.2.nvda-addon",
+	"ckbl" => "https://github.com/abdel792/sayCurrentKeyboardLanguage/releases/download/v1.8/sayCurrentKeyboardLanguage-1.8.nvda-addon",
+	"cr-dev" => "columnsReview-3.0-dev.nvda-addon",
+	"crh-dev" => "https://files.derekriemer.com/crashHero-dev.nvda-addon",
+	"cs" => "https://github.com/sykesman/NVDAAddon-Clipspeak/releases/download/v1.0-Prerelease/clipspeak-1.0.nvda-addon",
+	"cont" => "https://github.com/derekriemer/consoleTimer/releases/download/v1.0/consoleTimer-1.0.nvda-addon",
+	"cua" => "controlUsageAssistant-2.4.nvda-addon",
+	"dw" => "https://github.com/abdel792/dayOfTheWeek/releases/download/v6.0/dayOfTheWeek-6.0.nvda-addon",
+	"dw-dev" => "https://github.com/abdel792/dayOfTheWeek/releases/download/v6.0/dayOfTheWeek-6.0.nvda-addon",
+	"dx" => "dropbox-4.3.nvda-addon",
+	"dx-dev" => "dropbox-4.3.nvda-addon",
+	"earia" => "https://github.com/jmdaweb/enhancedAria/releases/download/v2.2/enhancedAria-2.2.nvda-addon",
+	"em" => "https://github.com/nvdaes/eMule/releases/download/3.15/eMule-3.15.nvda-addon",
+	"em-dev" => "https://github.com/nvdaes/eMule/releases/download/3.15/eMule-3.15.nvda-addon",
+	"emo" => "https://github.com/nvdaes/emoticons/releases/download/8.2/emoticons-8.2.nvda-addon",
+	"emo-dev" => "https://github.com/nvdaes/emoticons/releases/download/8.2/emoticons-8.2.nvda-addon",
+	"emo-o" => "https://github.com/nvdaes/emoticons/releases/download/6.5/emoticons-6.5.nvda-addon",
+	"etn" => "easyTableNavigator-1.1.nvda-addon",
+	"etn-dev" => "easyTableNavigator-1.1.nvda-addon",
+	"ets" => "https://github.com/josephsl/enhancedTouchGestures/releases/download/18.11/enhancedTouchGestures-18.11.nvda-addon",
+	"ets-dev" => "https://github.com/josephsl/enhancedTouchGestures/releases/download/18.11/enhancedTouchGestures-18.11.nvda-addon",
+	"ew" => "extendedWinamp-1.1.nvda-addon",
+	"fh" => "https://github.com/nvdajp/focusHighlight/releases/download/5.3/focusHighlight-5.3.nvda-addon",
+	"fh-dev" => "https://github.com/nvdajp/focusHighlight/releases/download/5.3/focusHighlight-5.3.nvda-addon",
+	"gc" => "goldenCursor-3.2.nvda-addon",
+	"gc-dev" => "goldenCursor-3.2.nvda-addon",
+	"gl" => "https://github.com/nvdaaddons/nvdaaddons.github.io/wiki/guideLines",
+	"gwv" => "https://github.com/josephsl/goldWave/releases/download/18.10/goldwave-18.10.nvda-addon",
+	"gwv-dev" => "https://github.com/josephsl/goldWave/releases/download/18.07/goldwave-18.07.nvda-addon",
+	"it" => "instantTranslate-4.2-dev.nvda-addon",
+	"it-dev" => "instantTranslate-4.2-dev.nvda-addon",
+	"indentnav" => "https://github.com/mltony/nvda-indent-nav/releases/download/v1.3/IndentNav-1.3.nvda-addon",
+	"inputlock" => "https://github.com/jmdaweb/inputLock/releases/download/v1.4/inputLock-1.4.nvda-addon",
+	"lambda" => "https://github.com/lambda-nvda/lambdaNvda/releases/download/1.2.2/lambda-1.2.2.nvda-addon",
+	"lambda-dev" => "https://github.com/lambda-nvda/lambdaNvda/releases/download/1.2.2/lambda-1.2.2-dev.nvda-addon",
+	"mp3dc" => "https://github.com/abdel792/mp3DirectCut/releases/download/v4.2/mp3DirectCut-4.2.nvda-addon",
+	"mp3dc-dev" => "https://github.com/abdel792/mp3DirectCut/releases/download/v4.2/mp3DirectCut-4.2.nvda-addon",
+	"mirc" => "mirc-1.6.nvda-addon",
+	"moz" => "https://github.com/javidominguez/MozillaScripts/releases/download/1.5/Mozilla-1.5.nvda-addon",
+	"mush" => "mushClient-1.3.nvda-addon",
+	"nb" => "noBeepsSpeechMode-2.4.nvda-addon",
+	"nvda3208" => "addonUpdater-18.11.nvda-addon",
+	"nvda7857" => "screenCurtain-20180807.nvda-addon",
+	"nvdaremote" => "https://nvdaremote.com/remote-2.2.nvda-addon",
+	"objLoc" => "https://github.com/josephsl/objLocationTones/releases/download/18.10/objLocTones-18.10.nvda-addon",
+	"objPad" => "https://github.com/josephsl/objPad/releases/download/18.11/objPad-18.11.nvda-addon",
+	"objPad-dev" => "https://github.com/josephsl/objPad/releases/download/18.11/objPad-18.11.nvda-addon",
+	"ocr" => "https://www.nvaccess.org/files/nvda-addons/ocr_0.20120529.01.nvda-addon",
+	"pckbbrl" => "https://www.nvaccess.org/files/nvda-addons/pcKbBrl-2014.1.nvda-addon",
+	"pm" => "https://github.com/nvdaes/placeMarkers/releases/download/11.2/placeMarkers-11.2.nvda-addon",
+	"pm-dev" => "https://github.com/nvdaes/placeMarkers/releases/download/12.0-dev/placeMarkers-12.0-dev.nvda-addon",
+	"pm-o" => "https://github.com/nvdaes/placeMarkers/releases/download/10.2/placeMarkers-10.2.nvda-addon",
+	"qb-dev" => "https://www.nvaccess.org/files/nvda-addons/quickBooks2014-2014.4.7.nvda-addon",
+	"rccp" => "https://github.com/tuukkao/nvda-reviewCursorCopier/releases/download/1.1/reviewCursorCopier-1.1.nvda-addon",
+	"rccp-dev" => "https://github.com/tuukkao/nvda-reviewCursorCopier/releases/download/0.2-dev/reviewCursorCopier-0.2-dev.nvda-addon",
+	"rf" => "https://github.com/nvdaes/readFeeds/releases/download/5.2.1/readFeeds-5.2.1.nvda-addon",
+	"rf-dev" => "https://github.com/nvdaes/readFeeds/releases/download/5.2.1/readFeeds-5.2.1.nvda-addon",
+	"rf-o" => "https://github.com/nvdaes/readFeeds/releases/download/4.5/readFeeds-4.5.nvda-addon",
+	"rm" => "https://github.com/josephsl/resourceMonitor/releases/download/18.10/resourceMonitor-18.10.1.nvda-addon",
+	"rm-dev" => "https://github.com/josephsl/resourceMonitor/releases/download/18.10/resourceMonitor-18.10.1.nvda-addon",
+	"rs-dev" => "rapidSettings-2.0-dev.nvda-addon",
+	"rsy" => "https://github.com/nvdaes/reportSymbols/releases/download/4.3/reportSymbols-4.3.nvda-addon",
+	"rsy-dev" => "https://github.com/nvdaes/reportSymbols/releases/download/4.3/reportSymbols-4.3.nvda-addon",
+	"rsy-o" => "https://github.com/nvdaes/reportSymbols/releases/download/3.6/reportSymbols-3.6.nvda-addon",
+	"sentencenav" => "https://github.com/mltony/nvda-sentence-nav/releases/download/v1.1/SentenceNav-1.1.nvda-addon",
+	"spie" => "https://www.nvaccess.org/files/nvda-addons/speechPlayerInEspeak-0.4.nvda-addon",
+	"spl" => "https://github.com/josephsl/stationPlaylist/releases/download/18.11/stationPlaylist-18.11.1.nvda-addon",
+	"spl-lts18" => "https://github.com/josephsl/stationPlaylist/releases/download/18.11/stationPlaylist-18.09.5-lts.nvda-addon",
+	"spl-dev" => "https://www.josephsl.net/files/nvdaaddons/getupdate.php?file=spl-dev",
+	"sps" => "https://github.com/jscholes/nvda-speech-history/releases/download/v2017.05.13/speechHistory.nvda-addon",
+	"st" => "systrayList-3.0.nvda-addon",
+	"st-dev" => "systrayList-3.0.nvda-addon",
+	"sws" => "https://github.com/tspivey/switchSynth/releases/download/v1.02/switchSynth-1.02.nvda-addon",
+	"spp" => "https://github.com/tspivey/speakPasswords/releases/download/v1.01/speakPasswords-1.01.nvda-addon",
+	"tbx" => "toolbarsExplorer-1.0-20181101-dev.nvda-addon",
+	"tbx-dev" => "toolbarsExplorer-1.0-20181101-dev.nvda-addon",
+	"tmast" => "https://github.com/nvdaaddons/toneMaster/releases/download/v1.3/toneMaster-1.3.nvda-addon",
+	"tod-dev" => "https://files.derekriemer.com/tipOfTheDay-dev.nvda-addon",
+	"tod" => "https://files.derekriemer.com/tipOfTheDay.nvda-addon",
+	"tv" => "teamViewer-2.3.nvda-addon",
+	"tv-dev" => "teamViewer-3.0-dev.nvda-addon",
+	"txtinfo" => "https://github.com/cartertemm/text_information/releases/download/1.0/textInformation-1.0.nvda-addon",
+	"ubi" => "unicodeBrailleInput-2.9.nvda-addon",
+	"ubi-dev" => "unicodeBrailleInput-2.9.nvda-addon",
+	"VR" => "virtualRevision-2.2.nvda-addon",
+	"vlc" => "vlc-1.3.nvda-addon",
+	"vlc-18" => "https://github.com/javidominguez/VLC/releases/download/2.4/VLC-2.4.nvda-addon",
+	"vlc-dev" => "https://github.com/javidominguez/VLC/releases/download/2.7dev/VLC-2.7dev.multilanguage.nvda-addon",
+	"vent" => "Ventrilo-1.0-dev.nvda-addon",
+	"w10" => "https://github.com/josephsl/wintenApps/releases/download/18.11/wintenApps-18.11.1.nvda-addon",
+	"w10-dev" => "https://www.josephsl.net/files/nvdaaddons/getupdate.php?file=w10-dev",
+	"wetp" => "http://www.nvda.it/files/plugin/weather_plus4.7.nvda-addon",
+	"wrd" => "word-0.2-dev.nvda-addon"
+);
+
+If (isset($_GET['file'])) {
+	$file = $_GET['file'];
+	$fullname = $addons[$file];
+
+	if ($fullname != "") {
+		Header('Location:'.$fullname);
+	} else {
+		header("HTTP/1.0 404 Not Found");
+		print "<h1>File not found</h1>";
+		print "<p>The file you requested could not be found on this server.</p>";
+		print "<p>Please check that the link that brought you here is correct and try again.</p>";
+		print "<p>Thanks</p>";
+	}
+} else If (isset($_GET['addonslist'])) {
+	echo json_encode($addons);
+} else {
+	print "<h1>Error:</h1>";
+	print "<p>Please check that the link that brought you here is correct and try again.</p>";
+	print "<p>If you continue to see this message report this error to the nvda addon list.</p>";
+	print "<p>Thanks</p>";
+}
+?>
